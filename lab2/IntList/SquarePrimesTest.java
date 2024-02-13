5cd50109e7cb90a115adefbb986cb522426ebe5f
tomato <2535981850@qq.com>
2024-02-13 13:52:23 +0800

    done with Arithmetic and IntListExercises and SquarePrimesTest

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,11 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+    @Test
+    public void testSquarePrimesSimple2() {
+        IntList lst = IntList.of(13, 15, 16, 17, 19);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("169 -> 15 -> 16 -> 289 -> 361", lst.toString());
+        assertTrue(changed);
+    }
 }
