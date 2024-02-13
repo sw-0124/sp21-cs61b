98a22d4f93bbed721c4127c63c910d5cd351cc78
tomato <2535981850@qq.com>
2024-02-13 14:05:16 +0800

    done with IntListExercises

--- lab2/IntList/IntListExercises.java
+++ lab2/IntList/IntListExercises.java
@@ -1,5 +1,6 @@
 package IntList;
 
+
 public class IntListExercises {
 
     /**
@@ -10,6 +11,9 @@ public class IntListExercises {
      */
     public static void addConstant(IntList lst, int c) {
         IntList head = lst;
+        if(head == null){
+            return;
+        }
         while (head.rest != null) {
             head.first += c;
             head = head.rest;
